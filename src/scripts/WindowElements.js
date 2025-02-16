0000d5a5f71c41bca967da82635b32ddf01b8f76
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-16 19:36:54 +0530

    Found modified or deleted file,  src/scripts/WindowElements.js. Committing Via Global Bash Script:git-status-automated.bat

--- src/scripts/WindowElements.js
+++ src/scripts/WindowElements.js
@@ -2,19 +2,33 @@ export class Modal {}
 
 export class Offcanvas {
   constructor() {
-    let offcanVasDiv = document.createElement("div");
-    offcanVasDiv.classList.add("offcanvas", "bg-dark", "text-light");
-    offcanVasDiv.id = "offcanvas";
-    offcanVasDiv.setAttribute("data-bs-backdrop", "static");
+    let offCanvasDiv = document.createElement("div");
+    offCanvasDiv.classList.add("offcanvas", "bg-dark", "text-light");
+    offCanvasDiv.id = "offcanvas";
+    offCanvasDiv.setAttribute("data-bs-backdrop", "static");
 
-    offcanVasDiv.setAttribute("tabindex", "-1");
-    offcanVasDiv.setAttribute(
+    offCanvasDiv.setAttribute("tabindex", "-1");
+    offCanvasDiv.setAttribute(
       "aria-labelledby",
       "movieSearchFormOffcanvasLabel"
     );
 
-    offcanVasDiv.innerHTML = `
-        
-    `
+    offCanvasDiv.innerHTML = `
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="movieSearchFormOffcanvasLabel">
+        TITLE LOADING...
+        </h5>
+        <button
+        type="button"
+        class="btn-close btn-close-white"
+        data-bs-dismiss="offcanvas"
+        aria-label="Close"
+        ></button>
+    </div>
+
+    <div class="offcanvas-body no-scrollbar">DATA LOADING...</div>
+    `;
+
+    document.body.appendChild(offCanvasDiv);
   }
 }
