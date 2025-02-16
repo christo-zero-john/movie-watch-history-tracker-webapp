d8a3e49f7aefa1d4387298c3fa8987045af56ff4
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-16 19:48:08 +0530

    Found modified or deleted file,  src/scripts/importer.js. Committing Via Global Bash Script:git-status-automated.bat

--- src/scripts/importer.js
+++ src/scripts/importer.js
@@ -8,8 +8,19 @@ window.offcanvas = new Offcanvas();
 window.offcanvas.show();
 window.offcanvas.setTitle("Movie Search Form");
 window.offcanvas.setContent(
-    `
-    
+  `
+        <form action="" class="">
+                <input
+                  type="text"
+                  class="col-9 col-md-10 bg-transparent border-0 border-bottom border-3 placeholder-light text-light fs-5 fw-100"
+                  placeholder="SEARCH FOR ANYTHING"
+                />
+                <button
+                  class="btn search-form-btn fs-5 fw-600 px-3 py-1 ms-2"
+                  type="submit"
+                >
+                  GO
+                </button>
+              </form>
     `
 );
-
