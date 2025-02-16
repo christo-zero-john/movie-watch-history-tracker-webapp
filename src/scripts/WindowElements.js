65b2db52496554c970da2ad3c399ceb0e21ec9a5
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-16 19:34:53 +0530

    Found modified or deleted file,  src/scripts/WindowElements.js. Committing Via Global Bash Script:git-status-automated.bat

--- src/scripts/WindowElements.js
+++ src/scripts/WindowElements.js
@@ -1,5 +1,20 @@
 export class Modal {}
 
 export class Offcanvas {
-    
+  constructor() {
+    let offcanVasDiv = document.createElement("div");
+    offcanVasDiv.classList.add("offcanvas", "bg-dark", "text-light");
+    offcanVasDiv.id = "offcanvas";
+    offcanVasDiv.setAttribute("data-bs-backdrop", "static");
+
+    offcanVasDiv.setAttribute("tabindex", "-1");
+    offcanVasDiv.setAttribute(
+      "aria-labelledby",
+      "movieSearchFormOffcanvasLabel"
+    );
+
+    offcanVasDiv.innerHTML = `
+        
+    `
+  }
 }
