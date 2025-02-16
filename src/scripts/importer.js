87baa6cd0c2a447db88963219e9ecb35ebceab77
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-16 17:59:58 +0530

    Found new file,  src/scripts/importer.js. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/scripts/importer.js
@@ -0,0 +1,7 @@
+import { DisplayManager } from "./DisplayManager.js";
+import { Modal, Offcanvas } from "./WindowElements.js";
+
+console.log("Importing DOM Handlers");
+window.displayManager = new DisplayManager();
+window.modal = new Modal();
+window.offcanvas = new Offcanvas();
