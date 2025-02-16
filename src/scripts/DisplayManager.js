2a14a61abe294a72a68e99f652eb9437cf10dee2
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-16 17:59:58 +0530

    Found new file,  src/scripts/DisplayManager.js. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/scripts/DisplayManager.js
@@ -0,0 +1,142 @@
+export class DisplayManager {
+  constructor() {
+    console.log("Display Manager is ready");
+  }
+
+  displayMyMovies() {
+    // This method is used to displays Movies in watch history
+    const myMovies = [];
+    myMovies.push(this.constructMovieItem());
+    console.log("Displaying Your Movies");
+    let html = "";
+    console.log(myMovies);
+    html += myMovies.map((movie, index) => {
+      movie.rating = this.constructStarRating(movie.rating);
+      return `
+            <div class="movie-item col-6 col-md-2 me-3">
+                <img
+                src="${movie.poster_image}"
+                alt="Cover Image"
+                class="cover-image img-fluid rounded"
+                />
+
+                <div class="hover-varient ps-2 rounded">
+                <div
+                    class="top-side w-fit m-3 ms-auto md-hd-50 d-flex flex-column justify-content-start align-items-center"
+                >
+                    <img
+                    src="/src/assets/images/icons/movie-in-watch-history.png"
+                    alt=""
+                    class="icon my-1"
+                    />
+                </div>
+
+                <p class="movie-name small">${movie.name}</p>
+                <div class="movie-date-time mt-1 d-flex flex-row">
+                    <p class="time me-2 small">
+                        ${movie.duration[0]}h 
+                        ${movie.duration[1]}m
+                    </p>
+                    <p class="date small">
+                        ${movie.release_date.join(" ")}
+                    </p>
+                </div>
+                <div
+                    class="rating mt-1 d-flex flex-row align-items-center justify-content-center w-fit"
+                >
+                ${movie.rating
+                  .map(
+                    (star) => `<img src="${star}" alt="" class="rating-star"/>`
+                  )
+                  .join("")}
+                </div>
+                <div
+                    class="genre d-flex flex-row justify-content-center align-items-center float-end mt-3 me-md-2"
+                >
+                    ${movie.genres
+                      .map(
+                        (genre) =>
+                          `<p class="mx-1 small text-orange">${genre}</p>`
+                      )
+                      .join("")}
+                </div>
+                </div>
+            </div>
+    `;
+    });
+    const myMoviesDiv = document.getElementById("my-movies");
+    myMoviesDiv.innerHTML = html;
+  }
+
+  displayMyGenre() {
+    // This method is used to display genre of movies in watch history
+    let myGenre = [
+      "Action",
+      "Adventure",
+      "Biography",
+      "Animation",
+      "Comedy",
+      "Documentary",
+      "Sci-Fi",
+      "Superhero",
+      "Fantasy",
+    ];
+
+    let html = "";
+    html = myGenre
+      .map(
+        (genre) =>
+          `<button class="genre-item border-0 m-2 px-3 py-2 rounded text-nowrap">
+        ${genre}
+        </button>`
+      )
+      .join("");
+
+    document.getElementById("my-genre").innerHTML = html;
+  }
+
+  constructMovieItem(movie) {
+    // This method is used to construct movie item
+    const movieItem = {
+      id: 317442,
+      name: "The Last: Naruto the Movie",
+      duration: 112,
+      release_date: ["2014", "DEC", "06"],
+      rating: 7.8,
+      poster_image: "/src/assets/images/temp/cover-image.jpg",
+      background_image: "",
+      genres: ["Animation", "Action", "Fantasy"],
+    };
+
+    return movieItem;
+  }
+
+  constructStarRating(rating) {
+    // This method is used to construct html of rating stars
+    console.log("Original rating: ", rating);
+    const fourStarRating = (rating / 10) * 4;
+    const stars = [];
+
+    for (let i = 0; i < 4; i++) {
+      const remainingValue = fourStarRating - i;
+      let starImage;
+
+      if (remainingValue >= 1) {
+        starImage = "/src/assets/images/rating-stars/100-star.png";
+      } else if (remainingValue >= 0.75) {
+        starImage = "/src/assets/images/rating-stars/80-star.png";
+      } else if (remainingValue >= 0.5) {
+        starImage = "/src/assets/images/rating-stars/50-star.png";
+      } else if (remainingValue >= 0.25) {
+        starImage = "/src/assets/images/rating-stars/40-star.png";
+      } else if (remainingValue < 0.25 && remainingValue > 0.1) {
+        starImage = "/src/assets/images/rating-stars/10-star.png";
+      } else {
+        starImage = "/src/assets/images/rating-stars/0-star.png";
+      }
+      stars.push(starImage);
+    }
+
+    return stars;
+  }
+}
