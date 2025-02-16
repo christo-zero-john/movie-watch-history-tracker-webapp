45cd76e484e2236a678e703442a048a8a431f350
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-16 20:06:52 +0530

    Found modified or deleted file,  src/scripts/importer.js. Committing Via Global Bash Script:git-status-automated.bat

--- src/scripts/importer.js
+++ src/scripts/importer.js
@@ -5,22 +5,5 @@ console.log("Importing DOM Handlers");
 window.displayManager = new DisplayManager();
 window.modal = new Modal();
 window.offcanvas = new Offcanvas();
-window.offcanvas.show();
-window.offcanvas.setTitle("Movie Search Form");
-window.offcanvas.setContent(
-  `
-        <form action="" class="">
-                <input
-                  type="text"
-                  class="col-9 col-md-10 bg-transparent border-0 border-bottom border-3 placeholder-light text-light fs-5 fw-100"
-                  placeholder="SEARCH FOR ANYTHING"
-                />
-                <button
-                  class="btn search-form-btn fs-5 fw-600 px-3 py-1 ms-2"
-                  type="submit"
-                >
-                  GO
-                </button>
-              </form>
-    `
-);
+window.modal.setContent("Modal Boady");
+window.modal.show();
