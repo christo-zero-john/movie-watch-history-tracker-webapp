e3fa18a66d2e060d819585651660a02e312b66cf
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-16 21:35:41 +0530

    Found modified or deleted file,  src/scripts/importer.js. Committing Via Global Bash Script:git-status-automated.bat

--- src/scripts/importer.js
+++ src/scripts/importer.js
@@ -1,7 +1,9 @@
 import { DisplayManager } from "./DisplayManager.js";
 import { Modal, Offcanvas } from "./WindowElements.js";
+import { SearchMovie } from "./SearchMovie.js";
 
 console.log("Importing DOM Handlers");
 window.displayManager = new DisplayManager();
 window.modal = new Modal();
 window.offcanvas = new Offcanvas();
+window.searchMovie = new SearchMovie();
