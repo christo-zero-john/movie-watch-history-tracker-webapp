a1c63ca8a79be5441d877d83d69e35fb9dca52ce
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-17 20:34:01 +0530

    Found modified or deleted file,  src/scripts/SearchMovie.js. Committing Via Global Bash Script:git-status-automated.bat

--- src/scripts/SearchMovie.js
+++ src/scripts/SearchMovie.js
@@ -16,19 +16,20 @@ export class SearchMovie {
         <div class="content">
             <!-- movie search form -->
             <div class="form-section ms-md-5 mt-3">
-                <form action="" class="">
-                <input
-                    type="text"
-                    class="col-9 col-md-10 bg-transparent border-0 border-bottom border-3 placeholder-light text-light fs-5 fw-100"
-                    placeholder="SEARCH FOR ANYTHING"
-                />
-                <button
-                    class="btn search-form-btn fs-5 fw-600 px-3 py-1 ms-2"
-                    type="submit"
-                    id="search-movie-btn"
-                >
-                    GO
-                </button>
+                <form action="" class="" onsubmit="window.searchMovie.searchMovieHandler(event)">
+                    <input
+                        type="text"
+                        id="movie-search-term"
+                        class="col-9 col-md-10 bg-transparent border-0 border-bottom border-3 placeholder-light text-light fs-5 fw-100"
+                        placeholder="SEARCH FOR ANYTHING"
+                    />
+                    <button
+                        class="btn search-form-btn fs-5 fw-600 px-3 py-1 ms-2"
+                        type="submit"
+                        id="search-movie-btn"
+                    >
+                        GO
+                    </button>
                 </form>
 
                 <div
@@ -84,6 +85,14 @@ export class SearchMovie {
                 </button>
                 </div>
             </div>
+
+            <div class="search-results w-100" id="movie-search-result">
+              <h4 class="nothing col-md-7 op-07 mx-4 mx-md-auto mt-5 pt-5">
+                Search for something to appear here.... <br />
+                You can also choose what to search for in the options....
+              </h4>
+            </div>
+
         </div>    
     `);
 
@@ -106,5 +115,32 @@ export class SearchMovie {
     console.log(window.searchOptions);
   }
 
-  searchMovieHandler() {}
+  searchMovieHandler(event) {
+    event.preventDefault();
+    let searchTerm = document.getElementById("movie-search-term").value;
+    searchResultDiv.innerHTML = `Searching for ${searchTerm}...`;
+    if (searchTerm.length < 1) {
+      window.confirm("Search term too short. Atleast 1 character required");
+    } else {
+      let results = this.getResults(searchTerm);
+      this.displaySearchResults(results);
+    }
+  }
+
+  async getResults(searchTerm) {
+    // Fetch search results from tmdb
+    let response = ["", "", "", "", ""];
+    let results = [];
+    response.forEach((movie) => {
+      let movieItem = window.displayManager.constructMovieItem(movie);
+      results.push(movieItem);
+    });
+
+    this.displaySearchResults(results);
+  }
+
+  displaySearchResults(searchResults) {
+    console.log("Displaying Search Results: ", results);
+    const searchResultDiv = document.getElementById("movie-search-result");
+  }
 }
