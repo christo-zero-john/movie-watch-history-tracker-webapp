1491c746d73250a8501a88faa4f979516191d8ba
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-30 20:47:33 +0530

    Found file,  src/modules/searchMovie.js. Commit via automated bash script

--- src/modules/searchMovie.js
+++ src/modules/searchMovie.js
@@ -11,8 +11,16 @@ class searchMovie {
     const form = event.target;
     const formData = new FormData(form);
     const searchTerm = formData.get("search-term");
-    console.log(searchTerm);
-    const request = {}
+    console.log(searchTerm);    
+
+    const searchURL = `https://api.themoviedb.org/3/search/movie?query=${searchTerm}`;
+    const request = {
+      method: "GET",
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+      },
+    };
   };
 }
 export default new searchMovie();
