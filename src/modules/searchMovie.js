3beb1473ef411573293b24c2476e504076f7d664
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-30 20:43:20 +0530

    Found file,  src/modules/searchMovie.js. Commit via automated bash script

--- src/modules/searchMovie.js
+++ src/modules/searchMovie.js
@@ -6,7 +6,13 @@ class searchMovie {
     return searchMovie.instance;
   }
 
-  
-
+  searchForMovie = async (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const searchTerm = formData.get("search-term");
+    console.log(searchTerm);
+    const request = {}
+  };
 }
 export default new searchMovie();
