26142c30b0f24c14d188f44c11588c7b07e7bc28
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-04-30 20:43:20 +0530

    Found file,  src/components/dashboard/dashboard.jsx. Commit via automated bash script

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,8 +1,10 @@
+import searchMovie from "../../modules/searchMovie";
+
 export default function Dashboard() {
   return (
     <div>
-      <form>
-        <input type="text" required placeholder="Search for movies" />
+      <form onSubmit={searchMovie.searchForMovie}>
+        <input name="search-term" type="text" required placeholder="Search for movies" />
         <button>Search</button>
       </form>
     </div>
