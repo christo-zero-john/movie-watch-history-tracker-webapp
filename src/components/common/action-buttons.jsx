edac43b52c4e9f3825d3d9010d25f3ea666c0c08
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-01 22:59:23 +0530

    Found file,  src/components/common/action-buttons.jsx. Commit via automated bash script

--- src/components/common/action-buttons.jsx
+++ src/components/common/action-buttons.jsx
@@ -1,8 +1,48 @@
+import CoreActions from "../../modules/CoreActions";
 import LocalDatabase from "../../modules/LocalDatabase";
 
-export default function ActionButtons({ movieId }) {
+export default function ActionButtons({ movie }) {
   const watcHistory = LocalDatabase.getWatchHistory();
   const wishList = LocalDatabase.getWishList();
 
-  return watcHistory.includes(movieId)?<button></button>
+  return (
+    <>
+      {
+        // If movie is in watch history, return 'remove' button else 'add' button.
+        watcHistory.includes(movie) ? (
+          <button
+            className="btn btn-outline-danger rounded-0 p-0 px-1 fs-6 small"
+            onClick={() => CoreActions.removeFromWatchHistory(movie)}
+          >
+            -
+          </button>
+        ) : (
+          <button
+            className="btn btn-outline-success rounded-0 p-0 px-1 fs-6 small"
+            onClick={() => CoreActions.addToWatchHistory(movie)}
+          >
+            +
+          </button>
+        )
+      }
+      {
+        // If movie is in wish list, return 'remove' button else return 'add' button.
+        wishList.includes(movie) ? (
+          <button
+            className="btn btn-outline-warning rounded-0 p-0 px-1 fs-6 small"
+            onClick={() => CoreActions.removeFromWishList(movie)}
+          >
+            ★
+          </button>
+        ) : (
+          <button
+            className="btn btn-outline-primary rounded-0 p-0 px-1 fs-6 small"
+            onClick={() => CoreActions.addToWishList(movie)}
+          >
+            ☆
+          </button>
+        )
+      }
+    </>
+  );
 }
