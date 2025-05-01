80c9a3121f986469b0a91e62e570be84ac2f4615
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-01 23:28:18 +0530

    Found file,  src/components/routes/watch-history.jsx. Commit via automated bash script

--- src/components/routes/watch-history.jsx
+++ src/components/routes/watch-history.jsx
@@ -1,7 +1,13 @@
 import LocalDatabase from "../../modules/LocalDatabase";
 import DisplayMoviesList from "../common/display-movies-list";
+import NavBar from "../common/nav-bar";
 
 export default function WatchHistory() {
   const watchHistory = LocalDatabase.getWatchHistory();
-  return <DisplayMoviesList movies={watchHistory} />;
+  return (
+    <>
+      <NavBar />
+      <DisplayMoviesList movies={watchHistory} />
+    </>
+  );
 }
