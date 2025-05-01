991ae0d09a961d93aef7c6b3341df2d348451e7d
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-01 22:02:37 +0530

    Found file,  src/components/common/display-movies-list.jsx. Commit via automated bash script

--- src/components/common/display-movies-list.jsx
+++ src/components/common/display-movies-list.jsx
@@ -1,11 +1,13 @@
 export default function DisplayMoviesList({ movies }) {
   return (
-    <div className="search-results">
+    <div className="search-results d-flex flex-row gap-2 justify-content-center flex-wrap">
       {movies.map((movie) => {
         return (
-          <div key={movie.id}>
-            <h3>{movie.title}</h3>
-            <p>{movie.overview}</p>
+          <div key={movie.id} className="card m-2 p-3 shadow-sm col-12 col-md-4">
+            <h4>{movie.title}</h4>
+            <div className="buttons">
+              <button className="btn btn-outline-primary rounded-0 p-0 px-3 fs-6 small">Details</button>
+            </div>
           </div>
         );
       })}
