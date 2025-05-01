56663650b4bc928ecdfcf76f262c70a71ae4f94b
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-01 22:02:38 +0530

    Found file,  src/components/dashboard/dashboard.jsx. Commit via automated bash script

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -14,9 +14,9 @@ export default function Dashboard() {
   }, [searchResults]);
 
   return (
-    <div>
+    <div className="pb-3">
       <form
-        className="text-center"
+        className="text-center sticky-top p-2  shadow bg-light"
         onSubmit={(event) => {
           TMDB.searchMovie(
             Helpers.extractFormData(event, "search-term")["search-term"],
@@ -27,13 +27,16 @@ export default function Dashboard() {
         }}
       >
         <input
-          className="border-1 rounded p-2 col-9 col-md-6 border-warning text-primary"
+          className="border-1 rounded-1 p-2 col-9 col-md-6 border-warning text-primary"
           name="search-term"
           type="text"
           required
           placeholder="Search for movies"
         />
-        <button className="btn btn-primary" disabled={isSearching}>
+        <button
+          className="btn btn-primary rounded-0 px-4 mb-1 shadow"
+          disabled={isSearching}
+        >
           Search
         </button>
       </form>
@@ -59,18 +62,23 @@ export default function Dashboard() {
             <DisplayMoviesList movies={searchResults.results} />
             <div className="pagination"></div>
             {searchResults.page < searchResults.total_pages ? (
-              <button
-                className="btn btn-primary px-5 mx-auto"
-                onClick={() =>
-                  TMDB.searchMovie(
-                    searchResults.search_term,
-                    searchResults.page + 1,
-                    setSearchResults
-                  )
-                }
-              >
-                Load More...
-              </button>
+              <div className="alert alert-warning p-0 w-100 text-center rounded-0">
+                <p className=" d-inline-block m-0">
+                  Page {searchResults.page} of {searchResults.total_pages}
+                  <button
+                    className="btn btn-outline-success text-uppercase d-inline-block mx-2 px-5 rounded-0 shadow"
+                    onClick={() =>
+                      TMDB.searchMovie(
+                        searchResults.search_term,
+                        searchResults.page + 1,
+                        setSearchResults
+                      )
+                    }
+                  >
+                    Load More...
+                  </button>
+                </p>
+              </div>
             ) : (
               <p className="alert alert-warning p-0 text-center rounded-0">
                 End of Results
