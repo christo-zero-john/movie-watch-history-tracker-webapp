a3df4dae5e3e699d5f784af629975916f0336c8f
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-01 21:01:05 +0530

    Found file,  src/main.jsx. Commit via automated bash script

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/defaults.css";
 import AppRoutes from "./app-routes";
 
 createRoot(document.getElementById("root")).render(
