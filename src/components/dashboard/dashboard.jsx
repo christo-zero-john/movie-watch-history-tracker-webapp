b6a4920427a5abdd30c380119f6601f37fcf962a
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-01 20:31:32 +0530

    Found file,  src/components/dashboard/dashboard.jsx. Commit via automated bash script

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 import TMDB from "../../modules/TMDB";
 import Helpers from "../../modules/helpers";
 import DisplayMoviesList from "../common/display-movies-list";
+import { Pagination } from "react-bootstrap";
+import { useEffect } from "react";
 
 export default function Dashboard() {
   const [searchResults, setSearchResults] = useState(null);
+
+  useEffect(() => {
+    console.log("search results: ", searchResults);
+  }, [searchResults]);
 
   return (
     <div>
@@ -25,20 +31,35 @@ export default function Dashboard() {
         />
         <button>Search</button>
       </form>
-      {!searchResults ? (
-        <div>
-          <h2>Search for movies</h2>
-        </div>
-      ) : (
-        <div>
-          <h2>
-            Showing {searchResults.results.length} of{" "}
-            {searchResults.total_results} Results Found
-          </h2>
-          <DisplayMoviesList movies={searchResults.results} />
-          <div className="pagination"></div>
-        </div>
-      )}
+      {
+        // Display search results if they exist
+        !searchResults ? (
+          <div>
+            <h2>Search for movies</h2>
+          </div>
+        ) : (
+          <div>
+            <h2>
+              Showing {searchResults.results.length} of{" "}
+              {searchResults.total_results} Results Found
+            </h2>
+            <DisplayMoviesList movies={searchResults.results} />
+            <div className="pagination"></div>
+            <button
+              className=""
+              onClick={() =>
+                TMDB.searchMovie(
+                  searchResults.search_term,
+                  searchResults.page + 1,
+                  setSearchResults
+                )
+              }
+            >
+              Load More...
+            </button>
+          </div>
+        )
+      }
     </div>
   );
 }
