00a8dbac0a4da4a1c2d566468e748f7197182f71
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-01 22:12:34 +0530

    Found file,  src/modules/CoreActions.js. Commit via automated bash script

--- src/modules/CoreActions.js
+++ src/modules/CoreActions.js
@@ -6,6 +6,10 @@ class CoreActions {
     return CoreActions.instance;
   }
 
-  
+  addToWatchList(movieId) {
+    window.confirm("Are you sure you want to add this movie to your watchlist?")
+      ? console.log(`Movie with ID ${movieId} added to watchlist`)
+      : console.log("Action cancelled");
+  }
 }
 export default new CoreActions();
