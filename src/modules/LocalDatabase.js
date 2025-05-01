db3cbc6455097c76b211f9868789c87ea1886925
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-01 22:59:24 +0530

    Found file,  src/modules/LocalDatabase.js. Commit via automated bash script

--- src/modules/LocalDatabase.js
+++ src/modules/LocalDatabase.js
@@ -7,38 +7,40 @@ class LocalDatabase {
   }
 
   initialzeDatabase() {
-    console.log("Initializing local database");
+    // console.log("Initializing local database");
     if (!localStorage.getItem("watch-history")) {
-      console.log("No watch history found, creating an empty one.");
+      // console.log("No watch history found, creating an empty one.");
       localStorage.setItem("watch-history", JSON.stringify([]));
     }
     if (!localStorage.getItem("wish-list")) {
-      console.log("No wish list found, creating an empty one.");
+      // console.log("No wish list found, creating an empty one.");
       localStorage.setItem("wish-list", JSON.stringify([]));
     }
-    console.log("Local database initialized successfully.");
+    // console.log("Local database initialized successfully.");
   }
 
   getWatchHistory() {
-    console.log("Fetching watch history");
+    // console.log("Fetching watch history");
     return JSON.parse(localStorage.getItem("watch-history"));
   }
 
   getWishList() {
-    console.log("Fetching wish list");
+    // console.log("Fetching wish list");
     return JSON.parse(localStorage.getItem("wish-list"));
   }
 
   putWatchHistory(data) {
-    console.log("Saving new watch history");
+    // console.log("Saving new watch history");
     return JSON.stringify(
       localStorage.setItem("watch-history", JSON.stringify(data))
     );
   }
 
   putWishList(data) {
-    console.log("Saving new wish list");
-    return JSON.stringify(localStorage.setItem("wish-list", JSON.stringify(data)));
+    // console.log("Saving new wish list");
+    return JSON.stringify(
+      localStorage.setItem("wish-list", JSON.stringify(data))
+    );
   }
 }
 export default new LocalDatabase();
