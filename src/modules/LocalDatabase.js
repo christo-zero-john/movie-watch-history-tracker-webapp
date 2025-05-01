8035633cddca07d91f6835303a72ffa5b12b04e0
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-01 22:21:17 +0530

    Found file,  src/modules/LocalDatabase.js. Commit via automated bash script

--- src/modules/LocalDatabase.js
+++ src/modules/LocalDatabase.js
@@ -9,12 +9,36 @@ class LocalDatabase {
   initialzeDatabase() {
     console.log("Initializing local database");
     if (!localStorage.getItem("watch-history")) {
+      console.log("No watch history found, creating an empty one.");
       localStorage.setItem("watch-history", JSON.stringify([]));
     }
     if (!localStorage.getItem("wish-list")) {
+      console.log("No wish list found, creating an empty one.");
       localStorage.setItem("wish-list", JSON.stringify([]));
     }
     console.log("Local database initialized successfully.");
+  }
+
+  getWatchHistory() {
+    console.log("Fetching watch history");
+    return JSON.parse(localStorage.getItem("watch-history"));
+  }
+
+  getWishList() {
+    console.log("Fetching wish list");
+    return JSON.parse(localStorage.getItem("wish-list"));
+  }
+
+  putWatchHistory(data) {
+    console.log("Saving new watch history");
+    return JSON.parse(
+      localStorage.setItem("watch-history", JSON.stringify(data))
+    );
+  }
+
+  putWishList(data) {
+    console.log("Saving new wish list");
+    return JSON.parse(localStorage.setItem("wish-list", JSON.stringify(data)));
   }
 }
 export default new LocalDatabase();
