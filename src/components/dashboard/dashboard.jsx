d3aca22a6e6d98ff9a431a181d4d87332d302c40
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-01 10:04:19 +0530

    Found file,  src/components/dashboard/dashboard.jsx. Commit via automated bash script

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import searchMovie from "../../modules/searchMovie";
 
 export default function Dashboard() {
+  const [searchResults, setSearchResults] = useState(null);
+
   return (
     <div>
-      <form onSubmit={searchMovie.searchForMovie}>
-        <input name="search-term" type="text" required placeholder="Search for movies" />
+      <form onSubmit={(event) => searchMovie.searchForMovie(event)}>
+        <input
+          name="search-term"
+          type="text"
+          required
+          placeholder="Search for movies"
+        />
         <button>Search</button>
       </form>
     </div>
