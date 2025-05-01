4c718421cec0f0eb9bda1ae2527649f3bb5bc4f5
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-02 00:02:57 +0530

    Found file,  src/app-routes.jsx. Commit via automated bash script

--- src/app-routes.jsx
+++ src/app-routes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Dashboard from "./components/routes/search";
 import WatchHistory from "./components/routes/watch-history";
+import SearchMovie from "./components/routes/search";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<SearchMovie />} />
+        <Route path="/search" element={<SearchMovie />} />
         <Route path="/watch-history" element={<WatchHistory />} />
       </Routes>
     </BrowserRouter>
