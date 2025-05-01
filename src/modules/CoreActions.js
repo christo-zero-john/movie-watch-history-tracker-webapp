d713ee8c51a727890ba6a0a3b9623182ad5e83d2
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-01 22:33:04 +0530

    Found file,  src/modules/CoreActions.js. Commit via automated bash script

--- src/modules/CoreActions.js
+++ src/modules/CoreActions.js
@@ -1,3 +1,5 @@
+import LocalDatabase from "./LocalDatabase";
+
 class CoreActions {
   constructor() {
     if (!CoreActions.instance) {
@@ -6,10 +8,46 @@ class CoreActions {
     return CoreActions.instance;
   }
 
-  addToWatchList(movieId) {
-    window.confirm("Are you sure you want to add this movie to your watchlist?")
-      ? console.log(`Movie with ID ${movieId} added to watchlist`)
-      : console.log("Action cancelled");
+  addToWatchList(movie) {
+    const action = window.confirm(
+      `Are you sure you want to add the movie ${movie.title} to your watchlist?`
+    );
+    if (action) {
+      let watchHistory = LocalDatabase.getWatchHistory();
+      // Check whether movie already in watch history
+      if (watchHistory.includes(movie.id)) {
+        window.confirm(`${movie.title} already in watch history`);
+      } else {
+        watchHistory.push(movie.id);
+        console.log(watchHistory);
+        LocalDatabase.putWatchHistory(watchHistory);
+        console.log("Watch history updated:", LocalDatabase.getWatchHistory());
+        window.confirm("Successfully saved to watch history");
+      }
+    } else {
+      console.log("Action cancelled by user. Movie not save to watch history.");
+    }
+  }
+
+  addToWishList(movie) {
+    const action = window.confirm(
+      `Are you sure you want to add the movie ${movie.title} to your wish list?`
+    );
+    if (action) {
+      let wishList = LocalDatabase.getWishList();
+      // Check whether movie already in Wish List
+      if (wishList.includes(movie.id)) {
+        window.confirm(`${movie.title} already in Wish List`);
+      } else {
+        wishList.push(movie.id);
+        console.log(wishList);
+        LocalDatabase.putWishList(wishList);
+        console.log("Wish List updated:", LocalDatabase.getWishList());
+        window.confirm("Successfully saved to Wish List");
+      }
+    } else {
+      console.log("Action cancelled by user. Movie not save to Wish List.");
+    }
   }
 }
 export default new CoreActions();
