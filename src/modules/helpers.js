9bf0319503efaf5c9fefee7dcdbf0a9d47270dc7
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-01 10:33:51 +0530

    Found file,  src/modules/helpers.js. Commit via automated bash script

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -5,5 +5,14 @@ class Helpers {
     }
     return Helpers.instance;
   }
+
+  /**
+   * Extracts formData from a form submit event.
+   * @param {*} event An event object sent when a form submit event happens.
+   * @param {*} elementNames all extra parameters are the names of the form elements to extract. Pass the element names in the order you want them to be extracted.
+   */
+  extractFormData(event, [...elementNames]) {
+    
+  }
 }
 export default new Helpers();
