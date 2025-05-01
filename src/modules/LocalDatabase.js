dc4d07c90875971c3e3aedc7ab8b0a4fe599084d
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-01 22:12:40 +0530

    Found file,  src/modules/LocalDatabase.js. Commit via automated bash script

---
+++ src/modules/LocalDatabase.js
@@ -0,0 +1,16 @@
+class CoreActions {
+    constructor() {
+      if (!CoreActions.instance) {
+        CoreActions.instance = this;
+      }
+      return CoreActions.instance;
+    }
+  
+    addToWatchList(movieId) {
+      window.confirm("Are you sure you want to add this movie to your watchlist?")
+        ? console.log(`Movie with ID ${movieId} added to watchlist`)
+        : console.log("Action cancelled");
+    }
+  }
+  export default new CoreActions();
+  
