492bf68a03d74e32fc21cb016ae26f5f0ac87816
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-01 10:20:20 +0530

    Found file,  src/components/dashboard/dashboard.jsx. Commit via automated bash script

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import searchMovie from "../../modules/searchMovie";
+import TMDB from "../../modules/TMDB";
 
 export default function Dashboard() {
   const [searchResults, setSearchResults] = useState(null);
 
   return (
     <div>
-      <form onSubmit={(event) => searchMovie.searchForMovie(event)}>
+      <form onSubmit={(event) => TMDB.searchMovie(event, setSearchResults)}>
         <input
           name="search-term"
           type="text"
@@ -15,6 +15,28 @@ export default function Dashboard() {
         />
         <button>Search</button>
       </form>
+      {!searchResults ? (
+        <div>
+          <h2>Search for movies</h2>
+        </div>
+      ) : (
+        <div>
+          <h2>{searchResults.total_results} Results Found</h2>
+          <div className="search-results">
+            {searchResults.results.map((movie) => {
+              return (
+                <div key={movie.id}>
+                  <h3>{movie.title}</h3>
+                  <p>{movie.overview}</p>
+                </div>
+              );
+            })}
+          </div>
+          <div className="pagination">
+            
+          </div>
+        </div>
+      )}
     </div>
   );
 }
