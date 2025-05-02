691caa1aa0c825f531e49610930d8e20796b7f4f
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-02 09:08:59 +0530

    Found file,  src/components/routes/watch-history.jsx. Commit via automated bash script

--- src/components/routes/watch-history.jsx
+++ src/components/routes/watch-history.jsx
@@ -23,12 +23,14 @@ export default function WatchHistory() {
         // If watch history has some elements but movies does not have, then display a message.
         watchHistory.length > 0 ? (
           movies.length == 0 ? (
-            <p className="">Fetching movies from database</p>
+            <p className="alert alert-warning text-center">
+              Fetching movies from database
+            </p>
           ) : (
             <DisplayMoviesList movies={movies} />
           )
         ) : (
-          <p className="">
+          <p className="alert alert-danger text-center">
             No movies found. Add some movies to the list to display here.
           </p>
         )
