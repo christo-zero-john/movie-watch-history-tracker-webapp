f5a4b39c225e9872ac482f20aab36c2b3f74bba3
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-02 23:13:04 +0530

    Found file,  src/components/common/display-movies-list.jsx. Commit via automated bash script

--- src/components/common/display-movies-list.jsx
+++ src/components/common/display-movies-list.jsx
@@ -1,21 +1,27 @@
 import ActionButtons from "./action-buttons";
 
 export default function DisplayMoviesList({ movies }) {
+  function getRandomColor() {
+    const colors = ["light", "warning", "primary", "danger", "success", "info"];
+
+    return colors[Math.floor(Math.random() * 5)];
+  }
+
   return (
     <div className="search-results d-flex flex-row gap-2 justify-content-center flex-wrap col-12">
       {movies.map((movie) => {
         return (
           <div
             key={movie.id}
-            className="result-item col-4 m-2 pe-2 rounded d-flex flex-column justify-content-start align-items-center"
+            className={`card border border-${getRandomColor()} result-item col-2 m-2 rounded`}
           >
             <img
               src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-              alt=""
-              className="result-cover me-2"
+              alt={`poster-${movie.title}`}
+              className="img-fluid"
               loading="lazy"
             />
-            <div className="result-content">
+            <div className="text-start result-content p-2 small">
               <p className="movie-name fw-500">{movie.title}</p>
               <p className="date small">Dec 06 2014</p>
             </div>
