7e477ed7882f6ec76dfb39408635554593c9403e
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-02 22:21:55 +0530

    Found file,  src/components/dashboard/user-statistics.jsx. Commit via automated bash script

--- src/components/dashboard/user-statistics.jsx
+++ src/components/dashboard/user-statistics.jsx
@@ -7,9 +7,9 @@ export default function UserStatistics() {
   function oneLineValue(right, left) {
     //
     return (
-      <div class="stat-item small d-flex flex-row align-items-center w-fit mx-4">
-        <p class="stat-value text-nowrap fw-bold mx-2 fs-5">{left}</p>
-        <p class="stat-label text-nowrap">
+      <div className="stat-item small d-flex flex-row align-items-center w-fit mx-4">
+        <p className="stat-value text-nowrap fw-bold mx-2 fs-5">{left}</p>
+        <p className="stat-label text-nowrap">
           {right[0]}
           <br />
           {right[1]}
@@ -20,13 +20,13 @@ export default function UserStatistics() {
 
   function twoLineValues(right, left) {
     return (
-      <div class="stat-item small d-flex flex-row w-fit mx-4">
-        <p class="stat-value text-nowrap fw-bold mx-2 text-end">
+      <div className="stat-item small d-flex flex-row w-fit mx-4">
+        <p className="stat-value text-nowrap fw-bold mx-2 text-end">
           {left[0]}
           <br />
           {left[1]}
         </p>
-        <p class="stat-label text-nowrap">
+        <p className="stat-label text-nowrap">
           {right[0]}
           <br />
           {right[1]}
@@ -36,7 +36,7 @@ export default function UserStatistics() {
   }
 
   return (
-    <section class="stats-container rounded md-w-fit mx-3 mx-md-auto my-4 p-3 px-0 pb-0 pb-md-3 d-flex flex-row align-items-center flex-nowrap overflow-auto no-scrollbar">
+    <section className="stats-container rounded md-w-fit mx-3 mx-md-auto my-4 p-3 px-0 pb-0 pb-md-3 d-flex flex-row align-items-center flex-nowrap overflow-auto no-scrollbar">
       {twoLineValues(["TOTAL", "WATCH TIME"], ["120HRS", "42MINS"])}
       {oneLineValue(["MOVIES", "WATCHED"], 22)}
 
