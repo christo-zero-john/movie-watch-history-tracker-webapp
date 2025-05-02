5815d03641aa1475def0ffb946e5cd478f06e7f9
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-02 20:01:22 +0530

    Found file,  src/components/dashboard/user-statistics.jsx. Commit via automated bash script

--- src/components/dashboard/user-statistics.jsx
+++ src/components/dashboard/user-statistics.jsx
@@ -1,3 +1,49 @@
 export default function UserStatistics() {
-  return <div>UserStatistics</div>;
+  return (
+    <section class="stats-container rounded md-w-fit mx-3 mx-md-auto my-4 p-3 px-0 pb-0 pb-md-3 d-flex flex-row align-items-center flex-nowrap overflow-auto no-scrollbar">
+      <div class="stat-item small d-flex flex-row w-fit mx-4">
+        <p class="stat-value text-nowrap fw-bold mx-2">
+          120HRS
+          <br />
+          42MINS
+        </p>
+        <p class="stat-label text-nowrap">
+          TOTAL
+          <br />
+          WATCH TIME
+        </p>
+      </div>
+
+      <div class="stat-item small d-flex flex-row align-items-center w-fit mx-4">
+        <p class="stat-value text-nowrap fw-bold mx-2 fs-5">22</p>
+        <p class="stat-label text-nowrap">
+          MOVIES
+          <br />
+          WATCHED
+        </p>
+      </div>
+
+      <div class="stat-item small d-flex flex-row w-fit mx-4">
+        <p class="stat-value text-nowrap fw-bold mx-2 text-end">
+          15
+          <br />
+          DAYS
+        </p>
+        <p class="stat-label text-nowrap">
+          CURRENT
+          <br />
+          STREAK
+        </p>
+      </div>
+
+      <div class="stat-item small d-flex flex-row align-items-center w-fit mx-4">
+        <p class="stat-value text-nowrap fw-bold mx-2 fs-5">37</p>
+        <p class="stat-label text-nowrap">
+          GENRES
+          <br />
+          WATCHED
+        </p>
+      </div>
+    </section>
+  );
 }
