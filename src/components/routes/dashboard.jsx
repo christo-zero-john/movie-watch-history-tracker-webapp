6a65d3c4c753b218cbf6f2c72882d521f1783ee4
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-02 17:33:48 +0530

    Found file,  src/components/routes/dashboard.jsx. Commit via automated bash script

--- src/components/routes/dashboard.jsx
+++ src/components/routes/dashboard.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import LocalDatabase from "../../modules/LocalDatabase";
+
 export default function Dashboard() {
-  const [savedLists, setSavedLists] = null;
+  const [savedLists, setSavedLists] = useState(null);
+
+  useEffect(() => {
+    const watchHistory = LocalDatabase.getWatchHistory();
+    const wishList = LocalDatabase.getWishList();
+
+    /**
+     * The movieList only has an array of movie ID's. The movies are stored in the IndexDB using Dexie js. This can be done using the constructMoviesList of localDatabase class.
+     * So fetch movie details of each movie ID in the movieList and store them as an array of objects in the movies state.
+     */
+
+    (async () => {
+      let watchHistoryMovies = await LocalDatabase.constructMoviesList(
+        watchHistory
+      );
+      let wishListMovies = await LocalDatabase.constructMoviesList(wishList);
+      console.log(watchHistoryMovies, wishListMovies);
+    })();
+  }, []);
 
   return !savedLists ? (
     <p className="alert alert-warning text-center">Fetching user data</p>
