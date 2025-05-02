ea210ef6709e0580e12e76e760626492745ed279
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-02 09:55:30 +0530

    Found file,  src/components/common/nav-bar.jsx. Commit via automated bash script

--- src/components/common/nav-bar.jsx
+++ src/components/common/nav-bar.jsx
@@ -3,14 +3,14 @@ import Navbar from "react-bootstrap/Navbar";
 
 export default function NavBar() {
   return (
-    <Navbar expand="md" className="px-2 shadow">
+    <Navbar expand="md" className="px-2 shadow border-bottom border-3 border-warning">
       <Navbar.Brand href="/">MWHT</Navbar.Brand>
       <Navbar.Toggle aria-controls="nav-bar" />
-      <Navbar.Collapse id="nav-bar">
+      <Navbar.Collapse id="nav-bar" className="alert alert-light rounded-0 border-0 shadow p-0 m-0 ps-2">
         <Nav className="ms-auto">
           <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/watch-history">Watch List</Nav.Link>
-          <Nav.Link href="/wish-list">Wish List</Nav.Link>
+          <Nav.Link href="/movie-list/watch-history">Watch List</Nav.Link>
+          <Nav.Link href="/movie-list/wish-list">Wish List</Nav.Link>
           <Nav.Link href="/search">Search for Movies</Nav.Link>
         </Nav>
       </Navbar.Collapse>
