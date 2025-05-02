9e6ead4ea0d48bdf62689e93ab30b8c7bc89abd5
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-02 09:23:51 +0530

    Found file,  src/modules/CoreActions.js. Commit via automated bash script

--- src/modules/CoreActions.js
+++ src/modules/CoreActions.js
@@ -20,9 +20,12 @@ class CoreActions {
         window.confirm(`${movie.title} already in watch history`);
       } else {
         watchHistory.push(movie.id);
-        console.log(watchHistory);
+
+        // console.log(watchHistory);
+
         LocalDatabase.putWatchHistory(watchHistory);
-        console.log("Watch history updated:", LocalDatabase.getWatchHistory());
+        console.log(`Movie ${movie.id} Successfully saved to watch history`);
+
         window.confirm("Successfully saved to watch history");
       }
     } else {
@@ -40,9 +43,12 @@ class CoreActions {
       if (watchHistory.includes(movie.id)) {
         // Fiter out all movie ID's except the one to be removed. This is done to avoid mutating the original array and to ensure that we only remove the specific movie ID
         watchHistory = watchHistory.filter((id) => id !== movie.id);
-        console.log(watchHistory);
+
+        // console.log(watchHistory);
+
         LocalDatabase.putWatchHistory(watchHistory);
-        console.log("Watch history updated:", LocalDatabase.getWatchHistory());
+        console.log(`Movie ${movie.id} removed from wishlist`);
+
         window.confirm("Successfully removed from watch history");
       } else {
         window.confirm(`${movie.title} not found in watch history`);
@@ -65,9 +71,11 @@ class CoreActions {
         window.confirm(`${movie.title} already in Wish List`);
       } else {
         wishList.push(movie.id);
-        console.log(wishList);
+
+        // console.log(wishList);
+
         LocalDatabase.putWishList(wishList);
-        console.log("Wish List updated:", LocalDatabase.getWishList());
+        console.log(`Movie ${movie.id} Successfully saved to Wish List`);
         window.confirm("Successfully saved to Wish List");
       }
     } else {
@@ -85,9 +93,12 @@ class CoreActions {
       if (wishList.includes(movie.id)) {
         // Fiter out all movie ID's except the one to be removed. This is done to avoid mutating the original array and to ensure that we only remove the specific movie ID
         wishList = wishList.filter((id) => id !== movie.id);
-        console.log(wishList);
+
+        // console.log(wishList);
+
         LocalDatabase.putWishList(wishList);
-        console.log("Wish List updated:", LocalDatabase.getWishList());
+        console.log(`Movie ${movie.id} removed from wishlist`);
+
         window.confirm("Successfully removed from Wish List");
       } else {
         window.confirm(`${movie.title} not found in Wish List`);
