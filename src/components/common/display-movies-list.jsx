82480e45cd7f9b330dda87b3bb08c14b03bbce13
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-02 23:00:44 +0530

    Found file,  src/components/common/display-movies-list.jsx. Commit via automated bash script

--- src/components/common/display-movies-list.jsx
+++ src/components/common/display-movies-list.jsx
@@ -7,7 +7,7 @@ export default function DisplayMoviesList({ movies }) {
         return (
           <div
             key={movie.id}
-            className="result-item col-4 m-2 pe-2 rounded d-flex flex-row justify-content-start align-items-center"
+            className="result-item col-4 m-2 pe-2 rounded d-flex flex-column justify-content-start align-items-center"
           >
             <img
               src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
@@ -18,12 +18,6 @@ export default function DisplayMoviesList({ movies }) {
             <div className="result-content">
               <p className="movie-name fw-500">{movie.title}</p>
               <p className="date small">Dec 06 2014</p>
-
-              <div className="genre d-flex flex-row justify-content-center align-items-center float-end mt-3 me-md-2">
-                <p className="mx-1 small text-orange">Action</p>
-                <p className="mx-1 small text-orange">Adventure</p>
-                <p className="mx-1 small text-orange">Romance</p>
-              </div>
             </div>
           </div>
         );
