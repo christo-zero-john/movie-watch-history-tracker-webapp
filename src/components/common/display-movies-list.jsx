25b8eb5c33df1e1908824ab2361f9b36d42a8ad5
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-02 16:56:55 +0530

    Found file,  src/components/common/display-movies-list.jsx. Commit via automated bash script

--- src/components/common/display-movies-list.jsx
+++ src/components/common/display-movies-list.jsx
@@ -2,20 +2,27 @@ import ActionButtons from "./action-buttons";
 
 export default function DisplayMoviesList({ movies }) {
   return (
-    <div className="search-results d-flex flex-row gap-2 justify-content-center flex-wrap">
+    <div className="search-results d-flex flex-row gap-2 justify-content-center flex-wrap col-12">
       {movies.map((movie) => {
         return (
           <div
             key={movie.id}
-            className="card m-2 p-3 shadow-sm col-12 col-md-4"
+            className="card rounded-0 p-2 shadow-sm col-12 col-md-3 d-flex flex-row gap-0"
           >
-            <h4>{movie.title}</h4>
-            <div className="buttons">
-              <button className="btn btn-outline-primary rounded-0 p-0 px-3 fs-6 small">
+            <img
+              src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+              alt=""
+              className="col-2 col-md-3"
+              lazy="true"
+            />
+
+            <div className="search-result__details col-9 ms-2 col-md-8">
+              <p className="fw-600">{movie.title}</p>
+              <button className="btn btn-outline-primary rounded-0 p-0 px-3 small-2">
                 Details
               </button>
-              <div className="action-buttons  float-end">
-                <ActionButtons  movie={movie} />
+              <div className="action-buttons float-end">
+                <ActionButtons movie={movie} />
               </div>
             </div>
           </div>
