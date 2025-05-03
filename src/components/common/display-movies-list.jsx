60fb6a02346039f9108ff3c125ae4e3e6d4fa5f3
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-03 11:27:34 +0530

    Found file,  src/components/common/display-movies-list.jsx. Commit via automated bash script

--- src/components/common/display-movies-list.jsx
+++ src/components/common/display-movies-list.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ActionButtons from "./action-buttons";
 import MovieSearchResultCard from "./movie-search-result-card";
 import { Modal, Button, Image } from "react-bootstrap";
+import ResultItemDetails from "../search/result-item-details";
 
 const monthInWords = {
   1: "January",
@@ -19,24 +20,30 @@ const monthInWords = {
 };
 
 export default function DisplayMoviesList({ movies }) {
-  const [showDetails, setShowDetails] = useState(false);
+  const [showDetails, setShowDetails] = useState(true);
 
   return (
-    <div className="search-results d-flex flex-row gap-2 justify-content-center flex-wrap col-12">
-      {movies.map((movie) => {
-        const releaseDate = movie.release_date.split("-");
-        // console.log(releaseDate[1]);
-        releaseDate[1] = monthInWords[+releaseDate[1]];
-        movie.release_date = releaseDate.reverse().join(" ");
-        // console.log(movie);
-        return (
-          <MovieSearchResultCard
-            setShow={setShowDetails}
-            movie={movie}
-            key={movie.id}
-          />
-        );
-      })}
-    </div>
+    <>
+      <ResultItemDetails
+        show={showDetails}
+        handleClose={() => setShowDetails(true)}
+      />
+      <div className="search-results d-flex flex-row gap-2 justify-content-center flex-wrap col-12">
+        {movies.map((movie) => {
+          const releaseDate = movie.release_date.split("-");
+          // console.log(releaseDate[1]);
+          releaseDate[1] = monthInWords[+releaseDate[1]];
+          movie.release_date = releaseDate.reverse().join(" ");
+          // console.log(movie);
+          return (
+            <MovieSearchResultCard
+              setShow={setShowDetails}
+              movie={movie}
+              key={movie.id}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 }
