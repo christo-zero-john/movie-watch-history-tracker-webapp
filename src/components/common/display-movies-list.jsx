003ebceeab45c717bc70fb9ed420ec9a7e33655a
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-03 08:57:23 +0530

    Found file,  src/components/common/display-movies-list.jsx. Commit via automated bash script

--- src/components/common/display-movies-list.jsx
+++ src/components/common/display-movies-list.jsx
@@ -1,19 +1,6 @@
 import ActionButtons from "./action-buttons";
 
-const monthInWords = {
-  1: "January",
-  2: "February",
-  3: "March",
-  4: "April",
-  5: "May",
-  6: "June",
-  7: "July",
-  8: "August",
-  9: "September",
-  10: "October",
-  11: "November",
-  12: "December",
-};
+
 
 export default function DisplayMoviesList({ movies }) {
   function getRandomColor() {
