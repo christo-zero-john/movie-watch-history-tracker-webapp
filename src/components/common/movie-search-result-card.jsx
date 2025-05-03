4bba9048e8e023502eb5c806816e7866d9c95f6b
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-03 08:57:23 +0530

    Found file,  src/components/common/movie-search-result-card.jsx. Commit via automated bash script

--- src/components/common/movie-search-result-card.jsx
+++ src/components/common/movie-search-result-card.jsx
@@ -1,3 +1,23 @@
-export default function MovieSearchResultCard() {
+const monthInWords = {
+  1: "January",
+  2: "February",
+  3: "March",
+  4: "April",
+  5: "May",
+  6: "June",
+  7: "July",
+  8: "August",
+  9: "September",
+  10: "October",
+  11: "November",
+  12: "December",
+};
+
+export default function MovieSearchResultCard({ movie }) {
+  const releaseDate = movie.release_date.split("-");
+  console.log(releaseDate[1]);
+  releaseDate[1] = monthInWords[+releaseDate[1]];
+  movie.release_date = releaseDate.reverse().join(" ");
+  console.log(movie);
   return <div>MovieSearchResultCard</div>;
 }
