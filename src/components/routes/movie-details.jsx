2b93c8dfe3f2332e70aef49ecad6051bc4705976
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-04 19:58:47 +0530

    Found file,  src/components/routes/movie-details.jsx. Commit via automated bash script

--- src/components/routes/movie-details.jsx
+++ src/components/routes/movie-details.jsx
@@ -22,111 +22,100 @@ export default function MovieDetails() {
   return (
     <div className="">
       <NavBar active="explore" />
-      {movie ? (
-        <div className="content p-0 m-0">
-          <img
-            src="/src/assets/images/bg-slideshow/img-3.jpg"
-            alt=""
-            className="rounded movie-details-bg-img"
-          />
+      <div className="content p-0 m-0">
+        <img
+          src="/src/assets/images/bg-slideshow/img-3.jpg"
+          alt=""
+          className="rounded movie-details-bg-img"
+        />
 
-          <div className="ms-3 p-3">
-            <button className="m-3 btn">
-              <img
-                src="/src/assets/images/icons/go-back-icon.png"
-                alt=""
-                className="go-back-icon"
-              />
-            </button>
+        <div className="ms-3 p-3">
+          <button className="m-3 btn">
+            <img
+              src="/src/assets/images/icons/go-back-icon.png"
+              alt=""
+              className="go-back-icon"
+            />
+          </button>
+          {movie ? (
+            <>
+              <div className="movie-details-content col-md-8 p-md-3 rounded">
+                <h3 className="name text-nowrap px-2">
+                  {movie.title}{" "}
+                  <span className="text-uppercase fs-6 small op-07">
+                    [{movie.original_title}]
+                  </span>{" "}
+                </h3>
 
-            <div className="movie-details-content col-md-8 p-md-3 rounded">
-              <h3 className="name text-nowrap px-2">
-                The Last: Naruto the movie
-              </h3>
+                <div className="d-flex flex-row rating-date-time">
+                  <div className="movie-date-time mt-1 d-flex flex-row fs-5">
+                    <p className="time me-2">1h 52m</p>
+                    <p className="date">{movie.release_date}</p>
+                  </div>
+                </div>
 
-              <div className="d-flex flex-row rating-date-time">
-                <div className="movie-date-time mt-1 d-flex flex-row fs-5">
-                  <p className="time me-2">1h 52m</p>
-                  <p className="date">Dec 06 2014</p>
+                <p className="small movie-summary-description">
+                  {movie.overview}
+                </p>
+
+                <ActionButtons movie={movie} expand />
+
+                <div className="movie-details-genre rounded d-flex flex-row justify-content-start flex-nowrap overflow-auto no-scrollbar">
+                  {movie.genres.map((genre) => (
+                    <button className="text-orange border-0 m-2 px-3 py-2 rounded text-nowrap">
+                      {genre.name}
+                    </button>
+                  ))}
                 </div>
               </div>
 
-              <p className="small movie-summary-description">
-                Two years after the events of the Fourth Great Ninja War, the
-                moon that Hagoromo Otsutsuki created long ago to seal away the
-                Gedo Statue begins to descend towards the world, threatening to
-                become a meteor that would destroy everything on impact. Amidst
-                this crisis, a direct descendant of Kaguya Otsutsuki named
-                Toneri Otsutsuki attempts to kidnap Hinata Hyuga but ends up
-                abducting her younger sister Hanabi. Naruto and his allies now
-                mount a rescue mission before finding themselves embroiled in a
-                final battle to decide the fate of everything.
-              </p>
+              <div className="my-4">
+                <h2 className="fs-5 p-0 text-orange">Recommended Movies</h2>
+                <div className="recommended-movie-items p-3 rounded d-flex flex-row justify-content-start align-items-center flex-nowrap overflow-auto no-scrollbar">
+                  <div className="movie-item col-6 col-md-2 me-3">
+                    <img
+                      src="/src/assets/images/temp/cover-image.jpg"
+                      alt=""
+                      className="cover-image img-fluid rounded"
+                    />
 
-              <ActionButtons movie={movie} expand />
+                    <div className="hover-varient ps-2 rounded">
+                      <div className="top-side w-fit m-3 ms-auto md-hd-50 d-flex flex-column justify-content-start align-items-center">
+                        <img
+                          src="/src/assets/images/icons/add-to-watch-history.png"
+                          alt=""
+                          className="small-icon my-1"
+                        />
+                        <img
+                          src="/src/assets/images/icons/add-to-wishlist.png"
+                          alt=""
+                          className="small-icon my-1"
+                        />
+                      </div>
 
-              <div className="movie-details-genre rounded d-flex flex-row justify-content-start flex-nowrap overflow-auto no-scrollbar">
-                <button className="text-orange border-0 m-2 px-3 py-2 rounded text-nowrap">
-                  Action
-                </button>
-                <button className="text-orange border-0 m-2 px-3 py-2 rounded text-nowrap">
-                  Romance
-                </button>
-                <button className="text-orange border-0 m-2 px-3 py-2 rounded text-nowrap">
-                  Adventure
-                </button>
-                <button className="text-orange border-0 m-2 px-3 py-2 rounded text-nowrap">
-                  Fantasy
-                </button>
-              </div>
-            </div>
+                      <p className="movie-name small">
+                        The Last: Naruto the movie
+                      </p>
+                      <div className="movie-date-time mt-1 d-flex flex-row">
+                        <p className="time me-2 small">1h 52m</p>
+                        <p className="date small">Dec 06 2014</p>
+                      </div>
 
-            <div className="my-4">
-              <h2 className="fs-5 p-0 text-orange">Recommended Movies</h2>
-              <div className="recommended-movie-items p-3 rounded d-flex flex-row justify-content-start align-items-center flex-nowrap overflow-auto no-scrollbar">
-                <div className="movie-item col-6 col-md-2 me-3">
-                  <img
-                    src="/src/assets/images/temp/cover-image.jpg"
-                    alt=""
-                    className="cover-image img-fluid rounded"
-                  />
-
-                  <div className="hover-varient ps-2 rounded">
-                    <div className="top-side w-fit m-3 ms-auto md-hd-50 d-flex flex-column justify-content-start align-items-center">
-                      <img
-                        src="/src/assets/images/icons/add-to-watch-history.png"
-                        alt=""
-                        className="small-icon my-1"
-                      />
-                      <img
-                        src="/src/assets/images/icons/add-to-wishlist.png"
-                        alt=""
-                        className="small-icon my-1"
-                      />
-                    </div>
-
-                    <p className="movie-name small">
-                      The Last: Naruto the movie
-                    </p>
-                    <div className="movie-date-time mt-1 d-flex flex-row">
-                      <p className="time me-2 small">1h 52m</p>
-                      <p className="date small">Dec 06 2014</p>
-                    </div>
-
-                    <div className="genre d-flex flex-row justify-content-center align-items-center float-end mt-3 me-md-2">
-                      <p className="mx-1 small text-orange">Action</p>
-                      <p className="mx-1 small text-orange">Adventure</p>
-                      <p className="mx-1 small text-orange">Romance</p>
+                      <div className="genre d-flex flex-row justify-content-center align-items-center float-end mt-3 me-md-2">
+                        <p className="mx-1 small text-orange">Action</p>
+                        <p className="mx-1 small text-orange">Adventure</p>
+                        <p className="mx-1 small text-orange">Romance</p>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
+            </>
+          ) : (
+            <p className="">Fetching Details...</p>
+          )}
         </div>
-      ) : (
-        <p className="">Fetching Details</p>
-      )}
+      </div>
     </div>
   );
 }
