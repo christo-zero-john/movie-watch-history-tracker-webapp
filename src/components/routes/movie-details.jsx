c56a40d83b9ee2f699d745c4828d8eb55a7ddb32
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-04 18:40:35 +0530

    Found file,  src/components/routes/movie-details.jsx. Commit via automated bash script

--- src/components/routes/movie-details.jsx
+++ src/components/routes/movie-details.jsx
@@ -8,35 +8,37 @@ export default function MovieDetails() {
 
   const [movie, setMovie] = useState(null);
   return (
-    <div className="text-light">
+    <div className="">
       <NavBar active="explore" />
-      <div class="content p-0 m-0">
+      <div className="content p-0 m-0">
         <img
           src="/src/assets/images/bg-slideshow/img-3.jpg"
           alt=""
-          class="movie-details-bg-img rounded op-04"
+          className="rounded movie-details-bg-img"
         />
 
-        <div class="ms-3 p-3">
-          <button class="m-3 btn">
+        <div className="ms-3 p-3">
+          <button className="m-3 btn">
             <img
               src="/src/assets/images/icons/go-back-icon.png"
               alt=""
-              class="go-back-icon"
+              className="go-back-icon"
             />
           </button>
 
-          <div class="movie-details-content col-md-8 p-md-3 rounded">
-            <h3 class="name text-nowrap px-2">The Last: Naruto the movie</h3>
+          <div className="movie-details-content col-md-8 p-md-3 rounded">
+            <h3 className="name text-nowrap px-2">
+              The Last: Naruto the movie
+            </h3>
 
-            <div class="d-flex flex-row rating-date-time">
-              <div class="movie-date-time mt-1 d-flex flex-row fs-5">
-                <p class="time me-2">1h 52m</p>
-                <p class="date">Dec 06 2014</p>
+            <div className="d-flex flex-row rating-date-time">
+              <div className="movie-date-time mt-1 d-flex flex-row fs-5">
+                <p className="time me-2">1h 52m</p>
+                <p className="date">Dec 06 2014</p>
               </div>
             </div>
 
-            <p class="small movie-summary-description">
+            <p className="small movie-summary-description">
               Two years after the events of the Fourth Great Ninja War, the moon
               that Hagoromo Otsutsuki created long ago to seal away the Gedo
               Statue begins to descend towards the world, threatening to become
@@ -50,56 +52,56 @@ export default function MovieDetails() {
 
             <ActionButtons movie={movie} />
 
-            <div class="movie-details-genre rounded d-flex flex-row justify-content-start flex-nowrap overflow-auto no-scrollbar">
-              <button class="text-orange border-0 m-2 px-3 py-2 rounded text-nowrap">
+            <div className="movie-details-genre rounded d-flex flex-row justify-content-start flex-nowrap overflow-auto no-scrollbar">
+              <button className="text-orange border-0 m-2 px-3 py-2 rounded text-nowrap">
                 Action
               </button>
-              <button class="text-orange border-0 m-2 px-3 py-2 rounded text-nowrap">
+              <button className="text-orange border-0 m-2 px-3 py-2 rounded text-nowrap">
                 Romance
               </button>
-              <button class="text-orange border-0 m-2 px-3 py-2 rounded text-nowrap">
+              <button className="text-orange border-0 m-2 px-3 py-2 rounded text-nowrap">
                 Adventure
               </button>
-              <button class="text-orange border-0 m-2 px-3 py-2 rounded text-nowrap">
+              <button className="text-orange border-0 m-2 px-3 py-2 rounded text-nowrap">
                 Fantasy
               </button>
             </div>
           </div>
 
-          <div class="my-4">
-            <h2 class="fs-5 p-0 text-orange">Recommended Movies</h2>
-            <div class="recommended-movie-items p-3 rounded d-flex flex-row justify-content-start align-items-center flex-nowrap overflow-auto no-scrollbar">
-              <div class="movie-item col-6 col-md-2 me-3">
+          <div className="my-4">
+            <h2 className="fs-5 p-0 text-orange">Recommended Movies</h2>
+            <div className="recommended-movie-items p-3 rounded d-flex flex-row justify-content-start align-items-center flex-nowrap overflow-auto no-scrollbar">
+              <div className="movie-item col-6 col-md-2 me-3">
                 <img
                   src="/src/assets/images/temp/cover-image.jpg"
                   alt=""
-                  class="cover-image img-fluid rounded"
+                  className="cover-image img-fluid rounded"
                 />
 
-                <div class="hover-varient ps-2 rounded">
-                  <div class="top-side w-fit m-3 ms-auto md-hd-50 d-flex flex-column justify-content-start align-items-center">
+                <div className="hover-varient ps-2 rounded">
+                  <div className="top-side w-fit m-3 ms-auto md-hd-50 d-flex flex-column justify-content-start align-items-center">
                     <img
                       src="/src/assets/images/icons/add-to-watch-history.png"
                       alt=""
-                      class="small-icon my-1"
+                      className="small-icon my-1"
                     />
                     <img
                       src="/src/assets/images/icons/add-to-wishlist.png"
                       alt=""
-                      class="small-icon my-1"
+                      className="small-icon my-1"
                     />
                   </div>
 
-                  <p class="movie-name small">The Last: Naruto the movie</p>
-                  <div class="movie-date-time mt-1 d-flex flex-row">
-                    <p class="time me-2 small">1h 52m</p>
-                    <p class="date small">Dec 06 2014</p>
+                  <p className="movie-name small">The Last: Naruto the movie</p>
+                  <div className="movie-date-time mt-1 d-flex flex-row">
+                    <p className="time me-2 small">1h 52m</p>
+                    <p className="date small">Dec 06 2014</p>
                   </div>
 
-                  <div class="genre d-flex flex-row justify-content-center align-items-center float-end mt-3 me-md-2">
-                    <p class="mx-1 small text-orange">Action</p>
-                    <p class="mx-1 small text-orange">Adventure</p>
-                    <p class="mx-1 small text-orange">Romance</p>
+                  <div className="genre d-flex flex-row justify-content-center align-items-center float-end mt-3 me-md-2">
+                    <p className="mx-1 small text-orange">Action</p>
+                    <p className="mx-1 small text-orange">Adventure</p>
+                    <p className="mx-1 small text-orange">Romance</p>
                   </div>
                 </div>
               </div>
