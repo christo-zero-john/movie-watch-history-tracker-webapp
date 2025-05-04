3bd1e7dc265f670e49254d3fbe601ba5e4d748d2
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-04 19:58:59 +0530

    Found file,  src/modules/Helpers.js. Commit via automated bash script

---
+++ src/modules/Helpers.js
@@ -0,0 +1,34 @@
+class Helpers {
+  constructor() {
+    if (!Helpers.instance) {
+      Helpers.instance = this;
+    }
+    return Helpers.instance;
+  }
+
+  /**
+   * Extracts formData from a form submit event.
+   * @param {*} event An event object sent when a form submit event happens.
+   * @param {*} elementNames all extra parameters are the names of the form elements to extract. Pass the element names in the order you want them to be extracted.
+   */
+  extractFormData(event, ...elementNames) {
+    event.preventDefault();
+    console.log("Extracting form data.");
+    const form = event.target;
+    const formData = new FormData(form);
+    let extractedData = {};
+    elementNames.forEach((name) => {
+      extractedData[name] = formData.get(name);
+    });
+
+    console.log("Successfully extracted form data");
+    return extractedData;
+  }
+
+  getRandomColor() {
+    const colors = ["light", "warning", "primary", "danger", "success", "info"];
+
+    return colors[Math.floor(Math.random() * 5)];
+  }
+}
+export default new Helpers();
