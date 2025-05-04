3028030c27a48f68e9011938b07c517e8263bf98
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-04 18:40:36 +0530

    Found file,  src/main.jsx. Commit via automated bash script

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import "./styles/index.css";
 import AppRoutes from "./app-routes";
 import LocalDatabase from "./modules/LocalDatabase";
 
-LocalDatabase.initialzeDatabase();
+LocalDatabase.initializeDatabase();
+
+// Set the bg color and text color of the entire document so that it matches the dark theme
+document.body.style.backgroundColor = "#212529";
+document.body.style.color = "#f8f9fa";
 
 createRoot(document.getElementById("root")).render(<AppRoutes />);
