8f405e943ede33211502959019af29716fc34b8d
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-05 21:00:12 +0530

    Found file,  src/components/dashboard/user-statistics.jsx. Commit via automated bash script

--- src/components/dashboard/user-statistics.jsx
+++ src/components/dashboard/user-statistics.jsx
@@ -41,12 +41,18 @@ export default function UserStatistics() {
 
   return (
     <section className="stats-container rounded md-w-fit mx-3 mx-md-auto my-4 p-3 px-0 pb-0 pb-md-3 d-flex flex-row align-items-center flex-nowrap overflow-auto no-scrollbar">
-      {twoLineValues(["TOTAL", "WATCH TIME"], ["120HRS", "42MINS"])}
-      {oneLineValue(["MOVIES", "WATCHED"], 22)}
+      {twoLineValues(
+        ["TOTAL", "WATCH TIME"],
+        [`${userdata.watchTime[0]}HRS`, `${userdata.watchTime[1]}MINS`]
+      )}
+      {oneLineValue(["MOVIES", "WATCHED"], userdata.totalMoviesWatched)}
 
-      {twoLineValues(["CURRENT", "STREAK"], ["15", "DAYS"])}
+      {twoLineValues(
+        ["CURRENT", "STREAK"],
+        [`${userdata.currentStreak}`, "DAYS"]
+      )}
 
-      {oneLineValue(["GENRES", "WATCHED"], 37)}
+      {oneLineValue(["GENRES", "WATCHED"], userdata.genres.length)}
     </section>
   );
 }
