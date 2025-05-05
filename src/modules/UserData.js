a50e9cb1e9f5007bc8e58718f9b9bbdb35a5e49c
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-05 20:35:18 +0530

    Found file,  src/modules/UserData.js. Commit via automated bash script

--- src/modules/UserData.js
+++ src/modules/UserData.js
@@ -25,18 +25,29 @@ class UserData {
 
     // An async function that could fetch all movies in watch history and wishlist and save those in userdata
     (async () => {
+      // Fetch all movies in watch history and setup userData.data
       this.watchHistory = await CoreActions.getMoviesInWatchHistory();
       this.wishList = await CoreActions.getMoviesInWishList();
-      this.watchTime = 0;
+
       this.totalMovies = 0;
       this.currentStreak = 0;
       this.genres = 0;
       this.watchHistory.forEach((movie) => {
-        console.log(movie.runtime, helpers.constructRuntime(movie.runtime));
-        // 3.35
+        // Calculate total watch time of the user and set userdata.data.watchTime
+        const calculatedRuntime = helpers.constructRuntime(movie.runtime);
+        this.data.watchTime[0] += calculatedRuntime[0];
+        this.data.watchTime[1] += calculatedRuntime[1];
       });
 
-      console.log(this);
+      // After completeing the loop, watch time in minutes will be greater than 60 (Because we are just adding up minutes of all n movies into the watchTime[1], which stores total minutes). Hence, we should convert it to hours and minutes once again and save the new results as final watch time.
+      const calculatedRuntime = helpers.constructRuntime(
+        this.data.watchTime[1]
+      );
+      console.log(calculatedRuntime);
+
+      this.data.watchTime[0] += calculatedRuntime[0];
+      this.data.watchTime[1] = calculatedRuntime[1];
+      console.log(this.data.watchTime);
     })();
   }
 }
