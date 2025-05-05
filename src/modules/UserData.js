fd3f8b7554e4249f7ec4050a12ed122a8df8c5f8
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-05 19:10:10 +0530

    Found file,  src/modules/UserData.js. Commit via automated bash script

--- src/modules/UserData.js
+++ src/modules/UserData.js
@@ -3,7 +3,22 @@ class UserData {
     if (!UserData.instance) {
       UserData.instance = this;
     }
+
+    this.data = {
+      watchTime: [0, 0],
+      totalMovies: 0,
+      currentStreak: 0,
+      genres: [],
+    };
+this.watchHistory=
     return UserData.instance;
   }
+
+  calculate: {
+    watchTime: () => {},
+    totalMovies: () => {},
+    currentStreak: () => {},
+    genres: () => {},
+  };
 }
 export default new UserData();
