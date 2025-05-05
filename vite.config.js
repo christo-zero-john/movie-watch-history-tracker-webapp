7df1db155a75f1fcb22fbe7eef36067b994089ef
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-05 22:50:42 +0530

    Found file,  vite.config.js. Commit via automated bash script

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { allowedHosts: [] },
+  server: { allowedHosts: ["bdaf-117-230-59-129.ngrok-free.app"] },
 });
