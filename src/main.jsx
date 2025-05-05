5a8601cc29333db84136eb84ab49d763bc427806
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-05 21:15:12 +0530

    Found file,  src/main.jsx. Commit via automated bash script

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import AppRoutes from "./app-routes";
 import UserData from "./modules/UserData";
 
 // Setup userData. LocalDatabase is also initialized inside this method.
-UserData.initializeUserData();
+UserData
 
 // Set the bg color and text color of the entire document so that it matches the dark theme
 document.body.style.backgroundColor = "#212529";
