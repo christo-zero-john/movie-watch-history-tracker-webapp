ba90de18249b661450d15b112f8c3c9e4fda9c8c
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-05 23:42:41 +0530

    Found file,  src/components/common/movie-search-result-card.jsx. Commit via automated bash script

---
+++ src/components/common/movie-search-result-card.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import ActionButtons from "./action-buttons";
+import { Link } from "react-router";
+import Helpers from "../../modules/Helpers";
+
+export default function MovieSearchResultCard({ movie }) {
+  const [showActionButtons, setShowActionButtons] = useState(false);
+
+  movie.poster_path = Helpers.constructImagePath(movie.poster_path);
+
+  movie.release_date = Helpers.dateToInWords(movie.release_date);
+
+  //   console.log(movie);
+  return (
+    <div
+      className={`card border border-${Helpers.getRandomColor()} result-item col-8 col-md-3 col-lg-2 m-2 rounded column justify-content-between`}
+    >
+      <div className="search-result-item-top">
+        <img
+          src={movie.poster_path}
+          alt={`poster-${movie.title}`}
+          className="img-fluid search-result-poster rounded-top"
+          loading="lazy"
+        />
+        <ActionButtons movie={movie} />
+        <Link to={`/details/${movie.id}`}>
+          <img
+            src="/src/assets/images/icons/more-details.png"
+            alt="info"
+            className="more-details-icon"
+            loading="lazy"
+          />
+        </Link>
+      </div>
+      <div className="text-start result-content p-2 small">
+        <Link
+          href={`/details/${movie.id}`}
+          className="nav-link movie-name fw-500 pt-2"
+        >
+          {movie.title}
+        </Link>
+        <p className="date small op-07">{movie.release_date}</p>
+      </div>
+    </div>
+  );
+}
