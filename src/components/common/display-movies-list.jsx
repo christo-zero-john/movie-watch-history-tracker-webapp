c8abd4f6b4968139e84a8debb3207b7951e3041a
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-05 09:57:56 +0530

    Found file,  src/components/common/display-movies-list.jsx. Commit via automated bash script

--- src/components/common/display-movies-list.jsx
+++ src/components/common/display-movies-list.jsx
@@ -16,18 +16,22 @@ const monthInWords = {
   12: "December",
 };
 
-export default function DisplayMoviesList({ movies }) {
+export default function DisplayMoviesList({ movies, className }) {
   return (
     <>
-      <div className="search-results d-flex flex-row gap-2 justify-content-center flex-wrap col-12">
-        {movies.map((movie) => {
-          const releaseDate = movie.release_date.split("-");
-          // console.log(releaseDate[1]);
-          releaseDate[1] = monthInWords[+releaseDate[1]];
-          movie.release_date = releaseDate.reverse().join(" ");
-          // console.log(movie);
-          return <MovieSearchResultCard movie={movie} key={movie.id} />;
-        })}
+      <div className="">
+        <div
+          className={`search-results d-flex flex-row gap-2 justify-content-start w-fit mx-auto flex-wrap ${className}`}
+        >
+          {movies.map((movie) => {
+            const releaseDate = movie.release_date.split("-");
+            // console.log(releaseDate[1]);
+            releaseDate[1] = monthInWords[+releaseDate[1]];
+            movie.release_date = releaseDate.reverse().join(" ");
+            // console.log(movie);
+            return <MovieSearchResultCard movie={movie} key={movie.id} />;
+          })}
+        </div>
       </div>
     </>
   );
