23e75ced573e21dd10ed97d9d3f4eb3d08a67f27
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-05 19:05:20 +0530

    Found file,  src/modules/UserData.js. Commit via automated bash script

--- src/modules/UserData.js
+++ src/modules/UserData.js
@@ -1,48 +1,9 @@
-class Helpers {
+class UserData {
   constructor() {
-    if (!Helpers.instance) {
-      Helpers.instance = this;
+    if (!UserData.instance) {
+      UserData.instance = this;
     }
-    return Helpers.instance;
-  }
-
-  /**
-   * Extracts formData from a form submit event.
-   * @param {*} event An event object sent when a form submit event happens.
-   * @param {*} elementNames all extra parameters are the names of the form elements to extract. Pass the element names in the order you want them to be extracted.
-   */
-  extractFormData(event, ...elementNames) {
-    event.preventDefault();
-    console.log("Extracting form data.");
-    const form = event.target;
-    const formData = new FormData(form);
-    let extractedData = {};
-    elementNames.forEach((name) => {
-      extractedData[name] = formData.get(name);
-    });
-
-    console.log("Successfully extracted form data");
-    return extractedData;
-  }
-
-  getRandomColor() {
-    const colors = ["light", "warning", "primary", "danger", "success", "info"];
-
-    return colors[Math.floor(Math.random() * 5)];
-  }
-
-  constructImagePath(tmdbPath) {
-    if (!tmdbPath) {
-      tmdbPath = "/src/assets/images/icons/image-placeholder.png";
-    } else {
-      if (
-        !tmdbPath.includes("http") &&
-        !tmdbPath.includes("image-placeholder")
-      ) {
-        tmdbPath = `https://image.tmdb.org/t/p/w500/${tmdbPath}`;
-      }
-    }
-    return tmdbPath;
+    return UserData.instance;
   }
 }
-export default new Helpers();
+export default new UserData();
