4b4760a740e532d8648f24b3d3c3b9fb2c9bf1f7
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-05-07 09:20:51 +0530

    Found file,  src/components/dashboard/my-genre.jsx. Commit via automated bash script

--- src/components/dashboard/my-genre.jsx
+++ src/components/dashboard/my-genre.jsx
@@ -1,6 +1,6 @@
 export default function MyGenre({ genres }) {
   return (
-    <div class="genre-section col-11 mx-auto my-5 mb-4">
+    <div class="genre-section col-12 col-md-11 rounded-0 rounded-md-2 mx-auto my-3 mb-5 my-md-4">
       <h2 class="fs-6">Your Movie Genre</h2>
       <div
         class="my-genre p-3 p-md-4 rounded d-flex flex-row justify-content-start align-items-center flex-nowrap overflow-auto no-scrollbar"
